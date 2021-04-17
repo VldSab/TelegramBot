d6815e29225b83b5d8d9a065ab4cee398278ae22
Vladimir Saberullin <43676560+VldSab@users.noreply.github.com>
2021-04-17 20:43:21 +0300

    Update keyboard.py

--- tg_bot/keyboard.py
+++ tg_bot/keyboard.py
@@ -1,12 +1,5 @@
 import telebot
 
-# button_download = telebot.types.KeyboardButton('get list')
-
-# greet_kb = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-# greet_kb.add(button_download)
-
-# greet_kb1 = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True).add(button_download)
-
 button_download = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True).add(
     telebot.types.KeyboardButton('Получить список', request_contact=True)
 )
@@ -21,7 +14,6 @@ callback_buttons = telebot.types.InlineKeyboardMarkup().add(
     telebot.types.InlineKeyboardButton('Пройти тест', callback_data='content2'),
     telebot.types.InlineKeyboardButton('Статья', callback_data='content3'),
     telebot.types.InlineKeyboardButton('Сайт', callback_data='content4')
-
 )
 
 site_button = telebot.types.InlineKeyboardMarkup().add(
